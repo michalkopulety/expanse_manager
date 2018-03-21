0ad4e26a57ee5f97cef8de850ae7063cd8aa81dc
Michal Kopulety <michal.kopulety@sap.com>
2018-03-21 20:10:34 +0100

    Init

---
+++ expense-manager/server/server.js
@@ -0,0 +1,15 @@
+//server/server.js
+var express = require('express');
+var router = require('./routes/routes.js')
+var path = require('path');
+var bodyParser = require('body-parser');
+var app = express();
+var mongoose = require('mongoose');
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '../client'));
+app.use(express.static(path.join(__dirname, '../client')));
+app.use(bodyParser.json({ limit: '50mb' }));
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: false }));
+mongoose.connect('mongodb://Admin:admin@ds135820.mlab.com:35820/test_db');
+app.use('/', router);
+module.exports = app;
